c8d0ee953c2786aaa314a40af36e6aa237f0421b
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-12-01 20:53:03 -0800

    update app dir

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>2048</h1>
+      {/* Game board will be added here */}
+    </div>
+  );
+}
+
+export default App;
