08e38ea1ccba3bfde822634cadf45064ae8c9b45
devyn-miller <144622580+devyn-miller@users.noreply.github.com>
2024-12-01 21:14:30 -0800

    trying to fix delploy

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>2048</h1>
-      {/* Game board will be added here */}
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
+      <h1 className="text-4xl mb-8 text-center">2048</h1>
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        {/* Game board will be added here */}
+        <div className="text-xl">Game Coming Soon!</div>
+      </div>
     </div>
   );
 }
